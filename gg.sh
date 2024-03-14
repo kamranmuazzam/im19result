c03ee4396172e95cce4c588e2940f363c0686063
Kamran Moazzam Ansari <kamran@Kamrans-MacBook-Air.local>
2024-03-15 01:51:38 +0600

    0.333.0

--- gg.sh
+++ gg.sh
@@ -1,6 +1,6 @@
 git add --all
-git commit -m "0.222.0"
+git commit -m "0.333.0"
 git push -u origin main
 
-# git tag v0.222.0
-# git push origin v0.222.0
+# git tag v0.333.0
+# git push origin v0.333.0
