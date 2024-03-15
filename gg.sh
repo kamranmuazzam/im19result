743792a3aafd77393ca711a4f26ca17b7cdcc656
Kamran Moazzam Ansari <kamran@Kamrans-MacBook-Air.local>
2024-03-15 15:27:27 +0600

    0.156.0

--- gg.sh
+++ gg.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the version variable
-old_version="0.155.0"
-version="0.155.0"
+old_version="0.156.0"
+version="0.156.0"
 LC_CTYPE=C find . -maxdepth 3 -type f -exec perl -pi -e "s/$old_version/$version/g" {} +
 
 
 
-
+git pull
 git add --all
 git commit -m "$version"
 git push -u origin main
@@ -15,5 +15,5 @@ gh release create v$version --title "Version $version" --notes "releasing $versi
 gh release edit v$version --draft=false
 
 
-# git tag v0.155.0
-# git push origin v0.155.0
+# git tag v0.156.0
+# git push origin v0.156.0
