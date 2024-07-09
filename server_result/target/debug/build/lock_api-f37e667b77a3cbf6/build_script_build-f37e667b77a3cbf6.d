7d1048e7e6eb71bfcec712341187a6dd715b07d4
Kamran <kamranmuazzam@gmail.com>
2024-07-09 16:50:44 +0600

    checking to push

--- server_result/target/debug/build/lock_api-f37e667b77a3cbf6/build_script_build-f37e667b77a3cbf6.d
+++ server_result/target/debug/build/lock_api-f37e667b77a3cbf6/build_script_build-f37e667b77a3cbf6.d
@@ -1,5 +1,5 @@
-/Users/kamran/19r/server_result/target/debug/build/lock_api-f37e667b77a3cbf6/build_script_build-f37e667b77a3cbf6: /Users/kamran/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs
+/Users/kamran/im19result/server_result/target/debug/build/lock_api-f37e667b77a3cbf6/build_script_build-f37e667b77a3cbf6: /Users/kamran/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs
 
-/Users/kamran/19r/server_result/target/debug/build/lock_api-f37e667b77a3cbf6/build_script_build-f37e667b77a3cbf6.d: /Users/kamran/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs
+/Users/kamran/im19result/server_result/target/debug/build/lock_api-f37e667b77a3cbf6/build_script_build-f37e667b77a3cbf6.d: /Users/kamran/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs
 
 /Users/kamran/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs:
